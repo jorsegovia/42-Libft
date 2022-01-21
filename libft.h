1e36f49fd2530eef39339780e468c502379cbc23
jsegovia <jorge.segovia.gil@gmail.com>
2022-01-21 11:46:39 +0100

    Added strrchr.c to libft

--- libft.h
+++ libft.h
@@ -39,6 +39,7 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlcat(char *restrict dst, const char * restrict src, size_t dstsize);
 size_t	ft_strlcpy(char *restrict dst, const char * restrict src, size_t dstsize);
