f0598dd4168ac97b8f6ecbda374c4e0303192792
jsegovia <jorge.segovia.gil@gmail.com>
2022-01-27 11:49:17 +0100

    ft_atoi improvements

--- ft_atoi.c
+++ ft_atoi.c
@@ -30,15 +30,14 @@ int	ft_atoi(const char *str)
 
 	negative = 1;
 	res = 0;
-	while (*str == '\t' || *str == '\n' || *str == '\r'
-		|| *str == '\v' || *str == '\f' || *str == ' ')
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
 		++str;
-	if (*str == '-')
-		negative = -1;
-	else
-		negative = 1;
-	if (negative == -1 || *str == '+')
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			negative = -1;
 		++str;
+	}
 	while (*str && *str >= '0' && *str <= '9')
 	{
 		res = res * 10 + (*str - 48);
