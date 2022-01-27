6f92a586745b51147d311a8e9314d7bb53bf2c50
jsegovia <jorge.segovia.gil@gmail.com>
2022-01-27 11:14:32 +0100

    Fixed ft_memcpy behaviour with both params NULL

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -29,6 +29,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	count;
 
 	count = 0;
+	if (!dst && !src)
+		return (NULL);
 	while (count < n)
 	{
 		*(unsigned char *)(dst + count) = *(unsigned char *)(src + count);
