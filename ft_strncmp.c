28c7b7e00e9bf9b6f27024c4cc64e7202520461b
jsegovia <jorge.segovia.gil@gmail.com>
2022-01-27 11:09:47 +0100

    Fixed and perfected ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -31,25 +31,16 @@ RETURN VALUES
 */
 #include "libft.h"
 
-static int	comp_char(char c1, char c2)
-{
-	if ((unsigned char)c1 != (unsigned char)c2)
-		return ((unsigned char)c1 - (unsigned char)c2);
-	return (0);
-}
-
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	c;
+	size_t c;
 
 	c = 0;
-	while (c < n && s1[c] && s2[c])
+	while (c < n && (s1[c] || s2[c]))
 	{
-		if (comp_char(s1[c], s2[c]))
-			return (s1[c] - s2[c]);
+		if (s1[c] != s2[c])
+			return ((unsigned char)s1[c] - (unsigned char)s2[c]);
 		c++;
 	}
-	if (c < n)
-		return (comp_char(s1[c], s2[c]));
 	return (0);
 }
