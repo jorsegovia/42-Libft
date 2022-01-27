b058d14b78dbb97f0360a24a0815bca31affc083
Jorseggil <jorge.segovia.gil@gmail.com>
2022-01-27 16:39:18 +0100

    Finished ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -25,13 +25,27 @@ External functs.
 Description
 	Applies the function ’f’ to each character of the string ’s’,
 	and passing its index as first argument to create a new string (with malloc(3))
-	resulting from successive applications of ’f
+	resultulting from successive applications of ’f
 */
 
 #include "libft.h"
-/*
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*result;
+	int	c;
 
+	if (!s)
+		return (NULL);
+	result = malloc(sizeof(char) * (ft_strlen(s) +1));
+	if (!result)
+		return (NULL);
+	c = 0;
+	while (s[i])
+	{
+		result[c] = f(c, (char *)s[c]);
+		i++;
+	}
+	result[i] = "\0";
+	return (result);
 }
-*/
