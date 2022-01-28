93656eab914321f7fc78c899da1911052e22b738
jsegovia <jorge.segovia.gil@gmail.com>
2022-01-28 12:22:13 +0100

    Created and tested ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -27,9 +27,56 @@ Description
 */
 
 #include "libft.h"
-/*
+
+static int	ft_getlen(long num)
+{
+	int	c;
+
+	c = 0;
+	if (num < 0)
+	{
+		num *= -1;
+		c++;
+	}
+	while (1)
+	{
+		num /= 10;
+		c++;
+		if (num == 0)
+			return (c);
+	}
+}
+
 char	*ft_itoa(int n)
 {
-
+	char	*str;
+	int	isneg;
+	size_t	len;
+	long    num;
+	
+	num = n;
+	if (num < 0)	//check for neg
+		isneg = 1;
+	else
+		isneg = 0;
+	len = ft_getlen(num);
+	str = malloc(sizeof(char) * (len + 1));	//assign memory
+	if (!str)
+		return (NULL);	//check memory assignation
+	str[len--] = '\0';	//write str end char
+	if (isneg)	//write neg if needed
+	{
+		str[0] = '-';
+		num = num * -1;
+	}
+	if (num == 0)
+	{
+		str[0] = '0';	//plan for n = 0
+	}
+	while (num > 0)	//run the conversion
+	{
+		str[len--] = num % 10 + '0';
+		num /= 10;
+	}
+	return (str);
 }
-*/
