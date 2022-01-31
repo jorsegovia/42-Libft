fb26bdc835bbcf6a24f90372b136ee82e3af75a3
Jorseggil <jorge.segovia.gil@gmail.com>
2022-01-31 09:15:52 +0100

    Created ft_split

--- ft_split.c
+++ ft_split.c
@@ -29,9 +29,79 @@ Description
 */
 
 #include "libft.h"
-/*
-char	**ft_split(char const *s, char c)
-{
 
+static void	memfree(char **res, int n)
+{
+	int count;
+
+	count = -1;
+	while (++count <= n)
+		free(res[count]);
+	free(res);
 }
-*/
+
+static char	*setword(char c, char const *s)
+{
+	int		count;
+	char	*word;
+
+	count = 0;
+	while (s[count] && c != s[count])
+		count++;
+	if (!(word = malloc(sizeof(char) * (count + 1))))
+		return (NULL);
+	count = -1;
+	while (s[++count] && c != s[count])
+		word[count] = s[count];
+	word[count] = '\0';
+	return (word);
+}
+
+static int	countword(char const *s, char c)
+{
+	int count;
+	int res;
+
+	count = 0;
+	res = 0;
+	while (s[count])
+	{
+		while (s[count] && c == s[count])
+			count++;
+		if (s[count] && c != s[count])
+			res++;
+		while (s[count] && c != s[count])
+			count++;
+	}
+	return (res);
+}
+
+char		**ft_split(char const *s, char c)
+{
+	int		count;
+	int		counter;
+	char	**res;
+
+	res = malloc(sizeof(char *) * (countword(s, c) + 1));
+	if (!s || !res)
+		return (NULL);
+	count = 0;
+	counter = 0;
+	while (s[count])
+	{
+		while (s[count] && c == s[count])
+			count++;
+		if (s[count] && c != s[count])
+		{
+			if ((res[counter++] = setword(c, &s[count])) == NULL)
+			{
+				memfree(res, counter - 1);
+				return (NULL);
+			}
+			while (s[count] && c != s[count])
+				count++;
+		}
+	}
+	res[counter] = 0;
+	return (res);
+}
