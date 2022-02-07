b766680e8dc44912b114bf9547b60560b523941a
jsegovia <jorge.segovia.gil@gmail.com>
2022-02-07 11:24:37 +0100

    Norm fixes

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -30,10 +30,10 @@ Description
 
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst || !del)
-		return;
+		return ;
 	(*del)(lst -> content);
 	free(lst);
 }
