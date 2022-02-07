25b8b880be4934e2ff64c7da5a21f918ff209646
jsegovia <jorge.segovia.gil@gmail.com>
2022-02-07 12:05:53 +0100

    Norm fixes

--- libft.h
+++ libft.h
@@ -10,9 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/errno.h>
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/errno.h>
 
 typedef struct s_list
 {
@@ -71,3 +74,5 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
+
+#endif
