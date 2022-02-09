b10e429fe72712825466d8f4259432bbaedac11a
jsegovia <jorge.segovia.gil@gmail.com>
2022-02-09 10:02:55 +0100

    tab fixes

--- ft_substr.c
+++ ft_substr.c
@@ -30,8 +30,7 @@ Description
 
 #include "libft.h"
 
-char
-	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
